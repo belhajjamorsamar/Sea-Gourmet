9843dbec1ce84495094227957579e50535062ac7
belhajjamorsamar <belhajjamorsamar@gmail.com>
2023-08-31 03:18:17 +0200

    navbarf

--- frontend/src/Components/Theme/Navbar.js
+++ frontend/src/Components/Theme/Navbar.js
@@ -1,76 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IonIcon } from '@ionic/react';
 import { menu } from 'ionicons/icons';
+import { FiX } from 'react-icons/fi';
 
 const Navbar = () => {
-	return (
-		<div>
-			<nav className="p-5 bg-white-600 shadow md:flex md:items-center md:justify-between">
-				<div className="flex justify-between items-center">
-					<img className="h-14 text-3xl inline" src="/images/logo.svg" alt="logo" />
+  const [open, setOpen] = useState(false);
 
-					<span className="text-3xl cursor-pointer mx-2 md:hidden block ">
-						<IonIcon icon={menu} />
-					</span>
-				</div>
+  const toggleMenu = () => {
+    setOpen(!open);
+  };
 
-				<div>
-					<ul
- className="md:flex md:items-center 
-  z-[-1] md:z-auto md:static absolute
- bg-white  w-full left-0 md:w-auto md:py-0 py-4 
- md:pl-0 pl-7  md:opacity-100 opacity-0  top-[-400px] 
- transition-all ease-in duration-500">
-						<li className="mx-3 my-6 md:my-0">
-							<a
-								href="/link"
-								className="text-xl hover:text-green-400 duration-400">
-								{' '}
-								HOME
-							</a>
-						</li>
-						<li className="mx-4 my-6 md:my-0">
-							<a
-								href="/link"
-								className="text-xl hover:text-green-400 duration-400">
-								{' '}
-								PRODUCTS
-							</a>
-						</li>
-						<li className="mx-4 my-6 md:my-0">
-							<a
-								href="/link"
-								className="text-xl hover:text-green-400 duration-400">
-								A PROPOS
-							</a>
-						</li>
-						<li className="mx-4 my-6 md:my-0">
-							<a
-								href="/link"
-								className="text-xl hover:text-green-400 duration-400">
-								{' '}
-								ABOUT US
-							</a>
-						</li>
-						<li className="mx-4 my-6 md:my-0">
-							<a
-								href="Home"
-								className="text-xl hover:text-green-400 duration-400">
-								{' '}
-								CONTACT US
-							</a>
-						</li>
-					</ul>
-				</div>
-			</nav>
-		</div>
-	);
+  return (
+    <nav className="bg-white shadow-lg">
+      <div className="container mx-auto px-6 py-3 md:flex md:items-center md:justify-between">
+        <div className="flex justify-between items-center">
+          <img className="h-12 md:h-14 text-2xl md:text-3xl inline" src="/images/logo.svg" alt="logo" />
+
+          <div className="md:hidden">
+            {open ? (
+              <span onClick={toggleMenu} className="text-2xl cursor-pointer">
+                <FiX size={24} />
+              </span>
+            ) : (
+              <span onClick={toggleMenu} className="text-2xl cursor-pointer">
+                <IonIcon icon={menu} name="menu" />
+              </span>
+            )}
+          </div>
+        </div>
+
+        <div className={`md:flex ${open ? 'block' : 'hidden'} mt-4 md:mt-0`}>
+          <ul className="md:flex md:items-center space-x-4 md:space-x-8">
+            <li>
+              <a href="/link" className="text-lg hover:text-green-400 transition duration-300">
+                HOME
+              </a>
+            </li>
+            <li>
+              <a href="/link" className="text-lg hover:text-green-400 transition duration-300">
+                PRODUCTS
+              </a>
+            </li>
+            <li>
+              <a href="/link" className="text-lg hover:text-green-400 transition duration-300">
+                A PROPOS
+              </a>
+            </li>
+            <li>
+              <a href="/link" className="text-lg hover:text-green-400 transition duration-300">
+                ABOUT US
+              </a>
+            </li>
+            <li>
+              <a href="Home" className="text-lg hover:text-green-400 transition duration-300">
+                CONTACT US
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
-
-
-
-function Menu(e){
-    e.name === 'menu' ? e.name="close" : e.name="menu"
-}
 
 export default Navbar;
