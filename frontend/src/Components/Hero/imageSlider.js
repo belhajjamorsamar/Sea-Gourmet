596d9d783ec391e93e3b1a6c7f60a90fab057b7a
belhajjamorsamar <belhajjamorsamar@gmail.com>
2023-09-02 23:26:57 +0200

    reglage slider component

---
+++ frontend/src/Components/Hero/imageSlider.js
@@ -0,0 +1,32 @@
+import MSG1 from '../assets/MSG1.png';
+import MSG2 from '../assets/MSG2.png';
+import MSG3 from '../assets/MSG3.png';
+import MSG4 from '../assets/MSG4.png';
+
+export default  [{
+    title :'First Slider Image' ,
+    description : "this is the first slider image of our carousel " ,
+    urls: MSG1 ,
+},{
+  
+    title :'Second Slider Image' ,
+    description : "this is the Second slider image of our carousel " ,
+    urls: MSG2 ,
+},
+
+{
+  
+    title :'Third Slider Image' ,
+    description : "this is the Third slider image of our carousel " ,
+    urls: MSG3 ,
+},
+
+
+{
+  
+    title :'Fourth Slider Image' ,
+    description : "this is the Four slider image of our carousel " ,
+    urls: MSG4 ,
+},
+
+];
