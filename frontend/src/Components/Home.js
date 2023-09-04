3bfe5a05dc5f1fdfe59218a6f2bfd7f71092d2ab
belhajjamorsamar <belhajjamorsamar@gmail.com>
2023-09-04 13:32:39 +0200

    amiloration

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import Slider from '../Components/Hero/Slider';
+import Cards from '../Components/SeaProducts/Cards';
+import { Link } from 'react-router-dom';
 
-import Slider from '../Components/Hero/Slider'
-import Cards from '../Components/SeaProducts/Cards'
 
-const Cards= [
+const Card = [
   { 
-    urls:""
+    name: 'SEA FOOD',
+    urls: '/images/F.jpg',
+  
   },
-  {};
-]
+  
+  {
+    name: 'Vegtables',
+    urls: '/images/F.jpg',
+  },
 
-
+  {
+    name: 'Fruit',
+    urls: '/images/F.jpg',
+  }
+];
 
 const Home = () => {
+ 
+  
   return (
     <div>
     <section className='pt-20'>
@@ -23,9 +35,31 @@ const Home = () => {
     </section>
 
 
-<section>
-
+ 
+<section className='py-10 px-40'>
+  <div className="flex items-center justify-center min-h-screen container mx-auto">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      {Card.map((product, index) => (
+        <div key={index} className="rounded-xl shadow-lg">
+          <div className="p-4 flex flex-col">
+            <div className="rounded-xl overflow-hidden h-[220px] w-full relative">
+              <img className="overflow-hidden" src={product.urls} alt={product.name} />
+              <Link to={`/your-link-here`} className="absolute inset-0 flex items-center justify-center bg-opacity-80">
+                <h5 className="text-2xl md:text-3xl font-medium text-center">
+                  <span className="underline text-white  text-bold text-7xl hover:text-green-500">
+                    {product.name}
+                  </span>
+                </h5>
+              </Link>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>
 </section>
+
+
 
 
 
