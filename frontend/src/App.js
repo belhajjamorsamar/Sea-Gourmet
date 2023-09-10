7758e3b329d42d424134b0171ffb82e3ca4536b1
belhajjamorsamar <belhajjamorsamar@gmail.com>
2023-09-11 01:53:04 +0200

    Admin structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,19 +5,36 @@ import Home from './Components/Home'
 import Products from './Components/Products'
 import Apropos from './Components/Apropos'
 import Contact from './Components/Contact'
-import {Routes ,Route} from 'react-router-dom'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import ListAdmin from './pages/admin/ListAdmin'
+
+
+import {Routes ,Route ,Outlet } from 'react-router-dom'
 const App = () => {
   return (
     <div>
-      <Navbar></Navbar>
-      <Routes>
-        <Route path="/"  exact element={<Home/>}/>
-        <Route path="/Products" element={<Products/>}/>
-        <Route path="/Apropos" element={<Apropos/>}/>
-        <Route path="/Contact" element={<Contact/>}/>
-      </Routes>
+        <Navbar />
      
-      <Footer></Footer>
+  
+     <Routes>
+      <Route path="/" exact element={<Home />} />
+      <Route path="/Products" element={<Products />} />
+      <Route path="/Apropos" element={<Apropos />} />
+      <Route path="/Contact" element={<Contact />} />
+    </Routes>
+    <Footer />
+
+
+     {/* Utilisez l'élément Outlet pour afficher les routes imbriquées */}
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/ListAdmin" element={<ListAdmin />} />
+      </Routes>
+  
+
+   
     </div>
   )
 }
---
+++ frontend/src/pages/admin/ListAdmin.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ListAdmin = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ListAdmin
