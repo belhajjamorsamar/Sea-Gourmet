4b9e2e1592848382d60cc4ce7a68b082e18473ec
belhajjamorsamar <belhajjamorsamar@gmail.com>
2023-09-10 14:44:39 +0200

    configure server

---
+++ BackOffice/main.js
@@ -0,0 +1,18 @@
+// imports
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+ 
+
+
+const app = express ();
+const PORT =process.env.PORT || 4000 ;
+
+app.get('/' ,(req,res) =>{
+    res.send('Hello world ... !')
+})
+
+app.listen (PORT , ()=>{
+    console.log(`Server started at http://localhost:${PORT}`);
+})
