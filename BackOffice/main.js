d4cd06a207d8e33060dea4a13a38a409411d270a
belhajjamorsamar <belhajjamorsamar@gmail.com>
2023-09-10 23:08:52 +0200

    data base configuration

--- BackOffice/main.js
+++ BackOffice/main.js
@@ -3,16 +3,20 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
- 
 
+const app = express();
+const PORT = process.env.PORT || 4000;
 
-const app = express ();
-const PORT =process.env.PORT || 4000 ;
+// database connection
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.log(error));
+db.once('open', () => console.log('Connected to the database!'));
 
-app.get('/' ,(req,res) =>{
-    res.send('Hello world ... !')
-})
+app.get('/', (req, res) => {
+  res.send('Hello world...!');
+});
 
-app.listen (PORT , ()=>{
-    console.log(`Server started at http://localhost:${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`);
+});
