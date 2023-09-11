b9fa1a4a1fd3fd9baf40b5d1ecb74f76d54f00b2
belhajjamorsamar <belhajjamorsamar@gmail.com>
2023-09-12 00:23:09 +0200

    some edits

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,36 @@
-import React from 'react'
-import Navbar from './Components/Theme/Navbar'
-import Footer from './Components/Theme/Footer'
-import Home from './Components/Home'
-import Products from './Components/Products'
-import Apropos from './Components/Apropos'
-import Contact from './Components/Contact'
-import Register from './pages/Register'
-import Login from './pages/Login'
-import ListAdmin from './pages/admin/ListAdmin'
+import React from 'react';
+import Navbar from './Components/Theme/Navbar';
+import Footer from './Components/Theme/Footer';
+import Home from './Components/Home';
+import Products from './Components/Products';
+import Apropos from './Components/Apropos';
+import Contact from './Components/Contact';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import ListAdmin from './pages/admin/ListAdmin';
 
-
-import {Routes ,Route ,Outlet } from 'react-router-dom'
+import { Routes, Route, Outlet } from 'react-router-dom';
 const App = () => {
-  return (
-    <div>
-        <Navbar />
-     
-  
-     <Routes>
-      <Route path="/" exact element={<Home />} />
-      <Route path="/Products" element={<Products />} />
-      <Route path="/Apropos" element={<Apropos />} />
-      <Route path="/Contact" element={<Contact />} />
-    </Routes>
-    <Footer />
+	return (
+		<div>
+			<Navbar />
 
+			<Routes>
+				<Route path="/" exact element={<Home />} />
+				<Route path="/Products" element={<Products />} />
+				<Route path="/Apropos" element={<Apropos />} />
+				<Route path="/Contact" element={<Contact />} />
+			</Routes>
+			<Footer />
 
-     {/* Utilisez l'élément Outlet pour afficher les routes imbriquées */}
-      <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/ListAdmin" element={<ListAdmin />} />
-      </Routes>
-  
+			{/* Utilisez l'élément Outlet pour afficher les routes imbriquées */}
+			<Routes>
+				<Route path="/register" element={<Register />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/ListAdmin" element={<ListAdmin />} />
+			</Routes>
+		</div>
+	);
+};
 
-   
-    </div>
-  )
-}
-
-export default App
+export default App;
